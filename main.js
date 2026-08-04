a1d8cf7a53310ba4acbd5bfa71d16d7068b9325b
ByronDev03 <byronjorge.158@gmail.com>
2026-08-04 00:53:51 -0600

    feat: design incomplete

---
+++ main.js
@@ -0,0 +1,33 @@
+let datos = [
+    {id: 1, nombre: 'Byron', edad: 23},
+    {id: 2, nombre: 'Danna', edad: 30}
+];
+
+//ELEMENTOS DEL DOM
+const form = document.getElementById('formulario');
+const idInput = document.getElementById('id');
+const
+const
+const
+const
+const
+
+// MOSTRAR DATOS (READ)
+function renderizar() {
+
+}
+
+// CREAR Y ACTUALIZAR (CREATE / UPDATE)
+form.addEventListener('submit', e => {
+
+});
+
+// EDITAR (DELETE)
+function editar(id){
+
+}
+
+// ELIMINAR (DELETE)
+function eliminar(id) {
+    
+}
