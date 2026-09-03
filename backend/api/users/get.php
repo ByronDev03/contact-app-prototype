7abf18fe54e3bedb63db3777037c0e703337049c
ByronDev03 <byronjorge.158@gmail.com>
2026-09-03 13:13:11 -0600

    feat: Improve user get endpoint validations

--- backend/api/users/get.php
+++ backend/api/users/get.php
@@ -7,6 +7,18 @@ header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../config/connection.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+
+    http_response_code(405);
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'Method not allowed.'
+    ]);
+
+    exit;
+
+}
 
 try {
 
